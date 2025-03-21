47bdd2d27fb3b9796bc14297afbd88997d7593d0
7acini <guilherme.facini@protonmail.com>
2025-03-20 22:46:04 -0300

    :recycle: refactor(database): Realizada refatoração no código aplicando melhores práticas na conexão e segurança do DB

---
+++ Models/Database/Database.php
@@ -0,0 +1,39 @@
+<?php 
+
+use PDO;
+use PDOException;
+
+class Database{
+
+    private static $instance = null;
+    private $pdo;
+    private $option = [
+        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+        PDO::ATTR_CASE => PDO::CASE_NATURAL,
+        PDO::ATTR_ORACLE_NULLS => PDO::NULL_EMPTY_STRING
+    ];
+
+
+    public function __construct() {
+        $hostname="127.0.0.1";
+	    $username="root";
+	    $password="";
+        $database="";
+
+        try {
+            $this->pdo = new PDO("mysql:host=$hostname;dbname=$database;charset=utf8", $username, $password, $this->option);
+        } catch (PDOException $e) {
+            die("Erro na conexão com o banco de dados: " . $e->getMessage());
+        }
+    }
+    
+    public function getInstance(){
+        if(self::$instance === null){
+            self::$instance = new self();
+        }
+        return self::$instance;
+    }
+    public function getConnection() {
+        return $this->pdo;
+    }
+}
