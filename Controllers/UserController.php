af05e52c35d80adddfd99fdcf98f2d75aca26039
7acini <guilherme.facini@protonmail.com>
2025-03-26 00:03:49 -0300

    :passport_control: feat(controllers): Cadastro funcionando normalmente, conectando e inserindo informações em nosso banco de dados

--- Controllers/UserController.php
+++ Controllers/UserController.php
@@ -1,6 +1,7 @@
 <?php
 
 require_once 'Utilitarios/RenderView.php';
+require_once 'Models/UserModel.php';
 
 class UserController {
     
@@ -14,6 +15,28 @@ class UserController {
 
     public function login() {
         RenderView::loadView('login');
+    }
+
+    public function cadastro() {
+        RenderView::loadView('/cadastro');
+    }
+
+    public function store() {
+        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+            $nome  = trim($_POST['nome']);
+            $email = trim($_POST['email']);
+            $senha = password_hash($_POST['senha'], PASSWORD_DEFAULT);
+
+            $userModel = new UserModel();
+            $success = $userModel->createUser($nome, $email, $senha);
+
+            if ($success) {
+                header("Location: /login");
+                exit;
+            } else {
+                echo "Erro ao cadastrar usuário!";
+            }
+        }
     }
 
 }
