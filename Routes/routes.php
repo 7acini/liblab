359113d5bb1dcd74cb13729267dea5dcaca04613
7acini <guilherme.facini@protonmail.com>
2025-03-25 23:46:00 -0300

    :construct: feat(routes): Realizada adição das novas rotas que lidaram com o processo de vizualização e processamento de cadastro

--- Routes/routes.php
+++ Routes/routes.php
@@ -1,7 +1,9 @@
 <?php 
 
 $routes = [
-    "/"           => "HomeController@index",
-    "/users/{id}" => "UserController@show",
-    "/login"      => "UserController@login"
+    "/"            => "HomeController@index",
+    "/users/{id}"  => "UserController@show",
+    "/login"       => "UserController@login",
+    "/cadastro"    => "UserController@cadastro",
+    "/register"    => "UserController@store", // Rota para processar o cadastro
 ];
